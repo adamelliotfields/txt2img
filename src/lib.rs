c36e8c17d4546d865f66de338401089a906ee327
Adam Fields <me@aef.me>
2025-02-20 15:08:32 -0500

    Rename util to utils

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 mod cli;
 mod client;
-mod config;
 mod services;
-mod util;
+mod utils;
 
 // Used in main
 pub use cli::Cli;
 pub use client::create_client;
-pub use config::get_or_init_config;
 pub use services::get_or_init_services;
-pub use util::{init_logger, write_image};
+pub use utils::{init_logger, write_image};
